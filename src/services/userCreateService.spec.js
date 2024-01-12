f88d6371da5be4715e74a7267c419e04ecb591da
gustavo <gmelo5805@gmail.com>
2024-01-12 17:13:07 -0300

    Knowing BeforeEach and Applying

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -3,6 +3,14 @@ const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
 const AppError = require("../utils/AppError");
 
 describe("UserCreateService", () => {
+  let userRepositoryInMemory = null;
+  let userCreateService = null;
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory();
+    userCreateService = new UserCreateService(userRepositoryInMemory);
+  });
+
   it("user should be create", async () => {
     const user = {
       name: "User Test",
@@ -10,10 +18,7 @@ describe("UserCreateService", () => {
       password: "123",
     };
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
     const userCreate = await userCreateService.execute(user);
-
     expect(userCreate).toHaveProperty("id");
   });
 
@@ -29,9 +34,6 @@ describe("UserCreateService", () => {
       email: "user@example.com",
       password: "456",
     };
-
-    const userRepository = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepository);
 
     await userCreateService.execute(user1);
     await expect(userCreateService.execute(user2)).rejects.toEqual(
